460808c4c34c4a5ddf689257edd02088ad0eb8b2
Kiran Challa <kichalla@microsoft.com>
2016-06-24 11:52:17 -0700

    updated

--- buildAgentSetpScriptUbuntu.sh
+++ buildAgentSetpScriptUbuntu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-trap 'exit' ERR
+trap 'exit' ERR # exit as soon as a command fails
 
 # NOTE: This script must be run with an account having 'sudo' privileges
 
@@ -12,10 +12,19 @@ if [ $# -ne 3 ]; then
 fi
 
 AGENTNAME=$1
-SERVERURL=$2
+SERVERURL=${2%/} # trim the final '/' in the string
 ASPNETOSNAME=$3
 
 apt-get update
+
+echo "Updating dhclient.conf..."
+echo 'supersede domain-name "redmond.corp.microsoft.com";' >> /etc/dhcp/dhclient.conf
+echo 'supersede domain-search "redmond.corp.microsoft.com";' >> /etc/dhcp/dhclient.conf
+echo 'supersede search "redmond.corp.microsoft.com";' >> /etc/dhcp/dhclient.conf
+
+echo "Restarting dhclient..."
+dhclient -r
+dhclient
 
 echo "Installing Git..."
 apt-get install -y git
@@ -23,7 +32,7 @@ apt-get install -y git
 # Sometimes git pull stalls, so this could fix it
 git config --global http.postBuffer 2M
 
-echo "Installing Mono 4.2.3..."
+echo "Installing Mono 4.4.0..."
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy/snapshots/4.4.0/. main" | tee /etc/apt/sources.list.d/mono-xamarin.list
 apt-get update
@@ -59,10 +68,10 @@ echo "Installing Nginx..."
 apt-get install -y nginx
 update-rc.d nginx defaults
 
-echo "Downloading build agent from http://aspnetci/ and updating the properties..."
+echo "Downloading build agent from $SERVERURL and updating the properties..."
 mkdir ~/TeamCity
 cd ~/TeamCity
-wget http://aspnetci/update/buildAgent.zip
+wget $SERVERURL/update/buildAgent.zip
 unzip buildAgent.zip
 cd bin
 chmod +x agent.sh
@@ -89,8 +98,6 @@ cat <<EOF >> agentStartStop
 # Short-Description: Start daemon at boot time
 # Description:       Enable service provided by daemon.
 ### END INIT INFO
-USER="aspnetagent"
-
 case "\$1" in
 start)
  sudo ~/TeamCity/bin/agent.sh start
